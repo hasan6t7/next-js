56716a9a585e957c92f44c74bae7c2202eaddeaf
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-19 12:22:56 +0600

    not found page

--- src/app/store/[[...slug]]/page.tsx
+++ src/app/store/[[...slug]]/page.tsx
@@ -1,14 +1,17 @@
+import { notFound } from "next/navigation";
 import React from "react";
 interface Props {
   params: Promise<{ slug: string[] }>;
 }
 const StoreDetails = async ({ params }: Props) => {
   const { slug } = await params;
-  console.log(slug);
+  if(slug?.length > 3){
+    notFound()
+  }
   return (
     <div>
       StoreDetails
-      {slug.length > 0 && slug.map((item, ind) => <p key={ind}>{item}</p>)}
+      {slug?.length > 0 && slug?.map((item, ind) => <p key={ind}>{item}</p>)}
     </div>
   );
 };
