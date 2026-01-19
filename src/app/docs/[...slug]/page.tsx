8a7ce845f88a466280ae84ff558463c6303429cf
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-19 11:17:13 +0600

    catch all segment

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const DocsDetails = () => {
-  return (
-    <div>Docs Details</div>
-  )
-}
+const DocsDetails = async ({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
+}) => {
+  const { slug } = await params;
+  console.log(slug)
+  if (slug.length === 2) {
+    return (
+      <p>
+        viewing document category {slug[0]} and document name {slug[1]}
+      </p>
+    );
+  }
+  else if(slug.length === 1 ){
+    return  <p>
+        viewing document category {slug[0]} 
+      </p>
+  }
+  return <div>Docs Details</div>;
+};
 
-export default DocsDetails
+export default DocsDetails;
