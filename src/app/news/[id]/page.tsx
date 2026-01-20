5409eb5c130edd77be0a3a27cac65e8c1e5d58b4
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-20 16:32:57 +0600

    dynamic metadata

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,8 +1,24 @@
+import { Metadata } from "next";
 import React from "react";
 
 interface ParamsProps {
   params: Promise<{ id: string }>;
 }
+
+export async function generateMetadata(
+  { params }: ParamsProps,
+
+): Promise<Metadata> {
+  const id = (await params).id
+ 
+  
+ 
+  return {
+    title: `News post ${id}`,
+    description: `News Post for ${id}`,
+  }
+}
+
 
 const NewsDetails = async ({ params }: ParamsProps) => {
   const { id } = await params;
