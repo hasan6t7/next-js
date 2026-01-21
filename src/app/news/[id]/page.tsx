cf8ff010208675e9696dff39845e38658b662faf
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-21 17:43:12 +0600

    search params

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -3,27 +3,35 @@ import React from "react";
 
 interface ParamsProps {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-export async function generateMetadata(
-  { params }: ParamsProps,
+export async function generateMetadata({
+  params,
+}: ParamsProps): Promise<Metadata> {
+  const id = (await params).id;
 
-): Promise<Metadata> {
-  const id = (await params).id
- 
-  
- 
   return {
     title: `News post ${id}`,
     description: `News Post for ${id}`,
-  }
+  };
 }
 
-
-const NewsDetails = async ({ params }: ParamsProps) => {
+const NewsDetails = async ({ params, searchParams }: ParamsProps) => {
   const { id } = await params;
-  console.log(id);
-  return <div>NewsDetails for id -{id}</div>;
+
+  return (
+    <div>
+      <div>NewsDetails for id -{id}</div>
+      <div>
+        {Object.entries(await searchParams).map(([key, value]) => (
+          <li key={key}>
+            {key} : {Array.isArray(value) ? value.join(", ") : value}
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NewsDetails;
