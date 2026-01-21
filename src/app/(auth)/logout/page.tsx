cdd5169f2befef0d3c3e7046f3133983c1bb21f1
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2026-01-21 19:48:09 +0600

    advanced navigation

---
+++ src/app/(auth)/logout/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+
+
+const Logout = () => {
+    const router = useRouter();
+  const handleLogOut = () => {
+    console.log("logout btn clicked");
+    router.push("/");
+  };
+  return (
+    <div>
+      <button
+        onClick={handleLogOut}
+        className="bg-gray-600 text-white px-5 py-1.5 rounded-xl border-none"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Logout;
